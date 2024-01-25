1f98f0bcf3df6d5b4e93ed538722d84a05a122df
MohammadAliChaudary <qaisch444@gmail.com>
2024-01-25 12:12:20 +0500

    how to structure data

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// How to structure data
+
+const example = [
+  {
+    name: "Jon",
+    lname: "Doe",
+    phone: 123456789,
+    email: "jon@gmail.com",
+  },
+  {
+    name: "Steve",
+    lname: "Smith",
+    phone: 123456789,
+    email: "steve@gmail.com",
+  },
+];
